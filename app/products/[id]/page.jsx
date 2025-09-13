17a5d175d1790bde6dce224e59c54a29ee3934ea
studiowalling <studiowalling@gmail.com>
2025-09-13 18:41:30 +0530

    small changes8

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -11,7 +11,35 @@ export default async function ProductPage({ params }) {
   if (!product) return notFound();
 
   return (
-    <div className="min-h-screen  bg-[#0D0F12] text-white"><section className="relative h-screen flex items-center justify-center bg-cover bg-no-repeat" style={{ backgroundImage: "url('/avban.png')" }} > {/* Gradient Overlay */} <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent"></div> {/* Container */} <div className="relative w-[85vw] md:w-[75vw] lg:w-[70vw] mx-auto flex flex-col text-left z-10 px-4 sm:px-6 lg:px-8"> {/* Heading */} <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-[70px] font-bold mb-6 leading-tight uppercase"> <span className="block text-white">Delivering </span> <span className="block text-white"> Precision & Power</span> <span className="block text-yellow-500">AVTEC TRANSMISSION</span> </h1> {/* Paragraph & Button */} <div className="max-w-3xl"> <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl text-gray-300 mb-8 leading-relaxed"> AVTEC transmission systems for heavy-duty machines.</p> </div> </div> </section>
+    <div className="min-h-screen  bg-[#0D0F12] text-white">
+  <section
+  className="
+    relative h-screen flex items-center justify-center
+    bg-cover bg-center bg-no-repeat
+  "
+  style={{ backgroundImage: "url('/avban.png')" }}
+>
+  {/* Gradient Overlay */}
+  <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent"></div>
+
+  {/* Container */}
+  <div className="relative w-[85vw] md:w-[75vw] lg:w-[70vw] mx-auto flex flex-col text-left z-10 px-4 sm:px-6 lg:px-8">
+    {/* Heading */}
+    <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-[70px] font-bold mb-6 leading-tight uppercase">
+      <span className="block text-white">Delivering</span>
+      <span className="block text-white">Precision & Power</span>
+      <span className="block text-yellow-500">AVTEC TRANSMISSION</span>
+    </h1>
+
+    {/* Paragraph */}
+    <div className="max-w-3xl">
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl text-gray-300 mb-8 leading-relaxed">
+        AVTEC transmission systems for heavy-duty machines.
+      </p>
+    </div>
+  </div>
+</section>
+
       {/* Title */}
       <div className="text-center mb-20 mt-20">
         <div className="flex gap-4 justify-center items-center">
