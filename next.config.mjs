0cb0d504977182abb9854c5ba221460a283ca821
studiowalling <studiowalling@gmail.com>
2025-09-15 19:46:27 +0530

    change1

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async redirects() {
-//     return [
-//       {
-//         source: '/:path*',
-//         has: [
-//           {
-//             type: 'host',
-//             value: 'setlite.in', // when someone visits setlite.in
-//           },
-//         ],
-//         destination: 'https://setlite.com/:path*', // send them to setlite.com
-//         permanent: true, // 308 Permanent Redirect
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
@@ -27,11 +7,11 @@ const nextConfig = {
         has: [
           {
             type: 'host',
-            value: 'setlite.com',
+            value: 'setlite.in', // when someone visits setlite.in
           },
         ],
-        destination: 'https://setlite.in/:path*',
-        permanent: true,
+        destination: 'https://setlite.com/:path*', // send them to setlite.com
+        permanent: true, // 308 Permanent Redirect
       },
     ];
   },
